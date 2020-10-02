dd28e87d16b837b9bc4ed102c9e1880f723d2442
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-10-02 17:03:21 -0400

    cleaned up class and got working examples
    cleaned out makeElement method
    
    fixes makeGraphRequest to always return array
    
    unified into one class again

--- scripts/index.class.php
+++ scripts/index.class.php
@@ -1,24 +1,24 @@
 <?php
 
-class BasicDisplay {
+class Instagram {
     const API_URL = 'https://graph.instagram.com/me/media?fields=';
 
     const API_FIELDS = 'caption,media_url,media_type,permalink,timestamp,username';
 
-    protected $access_token;
+    private $access_token;
 
-    protected $request_url;
+    private $request_url;
 
     public $modx;
 
-    public function __construct(\modX $modx, $token) {
+    public function __construct(\modX $modx, string $token) {
         $this->modx = $modx;
         $this->setToken($token);
         $this->setRequestUrl();
     }
 
     public function setRequestUrl() {
-        $this->request_url = self::API_URL . self::API_URL . '&access_token=' . $this->getToken();
+        $this->request_url = self::API_URL . self::API_FIELDS . '&access_token=' . $this->getToken();
     }
 
     public function getRequestUrl():string {
@@ -32,17 +32,14 @@ class BasicDisplay {
     public function getToken():string {
         return $this->access_token;
     }
-}
-
-class Instagram extends BasicDisplay {
-
+    
     /**
      * makeGraphRequest
      * Get the media from the Instagram Basic Display API
      *
-     * @return array | void
+     * @return array
      */
-    public function makeGraphRequest(){
+    public function makeGraphRequest():array{
         $rest_client = $this->modx->getService('rest', 'rest.modRest');
         $request = $this->getRequestUrl();
         $response = $rest_client->get($request);
@@ -53,13 +50,13 @@ class Instagram extends BasicDisplay {
 
         if (!empty($response_error)) {
             $this->modx->log(xPDO::LOG_LEVEL_ERROR, 'ERROR: _makeGraphRequest');
-            $this->modx->log(xPDO::LOG_LEVEL_ERROR, print_r($response_err));
-            return;
+            print_r($response_err);
+            return [];
         }
 
         if (empty($response_array)) {
             $this->modx->log(xPDO::LOG_LEVEL_ERROR, 'ERROR: _makeGraphRequest - No results returned');
-            return;
+            return [];
         }
 
         return $response_array['data'];
@@ -74,34 +71,20 @@ class Instagram extends BasicDisplay {
      * @param array $properties
      * @return void
      */
-    public function makeElement(&$cache, $tpl, string $type, array $properties = null) {
+    public function makeElement(string $tpl, string $type, array $properties = null) {
         $output = false;
-        $content = false;
         switch ($type) {
             case '@INLINE':
                 $uid = uniqid();
-                $chunk = $this->modx->newObject('modChunk', array('name' => "{$type}-{$uid}"));
+                $chunk = $this->modx->newObject('modChunk', ['name' => "{$type}-{$uid}"]);
                 $chunk->setCacheable(false);
                 $output = $chunk->process($properties, $tpl);
                 break;
             case '@CHUNK':
             default:
-                $chunk = null;
-                if (!isset($cache['@CHUNK'])) $cache['@CHUNK'] = [];
-                if (!array_key_exists($tpl, $cache['@CHUNK'])) {
-                    if ($chunk = $this->modx->getObject('modChunk', array('name' => $tpl))) {
-                        $cache['@CHUNK'][$tpl] = $chunk->toArray('', true);
-                    } else {
-                        $cache['@CHUNK'][$tpl] = false;
-                    }
-                } elseif (is_array($cache['@CHUNK'][$source])) {
-                    $chunk = $modx->newObject('modChunk');
-                    $chunk->fromArray($cache['@CHUNK'][$tpl], '', true, true, true);
-                }
-                if (is_object($chunk)) {
-                    $chunk->setCacheable(false);
-                    $output = $chunk-> process($properties);
-                }
+                $chunk = $this->modx->getObject('modChunk', ['name' => $tpl]);
+                $chunk->setCacheable(false);
+                $output = $chunk->process($properties);
                 break;
         }
         return $output;
@@ -110,32 +93,31 @@ class Instagram extends BasicDisplay {
     /**
      * makePhotos
      * Generates each instagram photo based on the template given
-     * @param [???] $tpl
+     * @param string $tpl
      * @param string $type
      * @param array $data
      * @param integer $max
      * @return string
      */
-    public function makePhotos($tpl, string $type, array $data, int $max = 8) {
-        static $tpl_cache = [];
+    public function makePhotos(string $tpl, string $type, array $data, int $max) {
         $i = 0;
         $output = [];
         foreach($data as $item) {
-            if ($item['media_type'] != 'IMAGE') continue;
-            if ($i === $max) break;
+            if ($item['media_type'] !== 'IMAGE') continue;
+            if ($i == $max) break;
             $i++;
 
-            $output[]= $this->makeTpl($tpl_cache, $tpl, $type, [
+            $output[]= $this->makeElement($tpl, $type, [
                 'idx' => $i,
                 'type' => $item['media_type'],
                 'src' => $item['media_url'],
                 'link' => $item['permalink'],
                 'caption' => $item['caption'],
             ]);
+            
         }
-
         $photos = implode('', $output);
-
+        
         return $photos;
     }
 
@@ -144,11 +126,11 @@ class Instagram extends BasicDisplay {
      * The instantiation function. Once the class has been constructed, call getPhotos to make the
      * necessary requests and create the templates
      * 
-     * @param [???] $tpl
+     * @param string $tpl // might not actually give a string, needs further testing
      * @param integer $max
      * @return string
      */
-    public function getPhotos($tpl, int $max) {
+    public function getPhotos(string $tpl, int $max) {
         if (empty($tpl)) return false;
 
         $type = '@CHUNK';
@@ -160,8 +142,7 @@ class Instagram extends BasicDisplay {
         }
 
         $response = $this->makeGraphRequest();
-
-        $elements = $this->makePhotos($type, $tpl, $response, $max);
+        $elements = $this->makePhotos($tpl, $type, $response, $max);
 
         return $elements;
     }
