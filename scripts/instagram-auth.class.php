d4467e6b5a14e7b54eb09ecf0b6e2489778581fd
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-12-07 18:03:17 -0500

    moves towards true oauth process
    adds -> refreshAccessToken
    ---
    This will use the active token in the system to get a "more"
    active token, one with a longer expiration date
    
    -> getLongLivedToken
    This takes the OAuth code and requests a
    Long Lived Token. This token has an expiration
    time of 2 months
    
    -> getLoginLink
    rewrites it to fix a weird php bug I introduced

--- scripts/instagram-auth.class.php
+++ scripts/instagram-auth.class.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * General Authorization Flow is as such:
+ * 
+ * Client logs in with the Auth Login: Link InstagramAuth->getLoginLink()
+ * After logging into Instagram with the window that pops up, it will redirect to the redirect
+ */
+
+
 class InstagramAuth {
     const API_OAUTH_URL = 'https://api.instagram.com/oauth/authorize';
 
@@ -9,7 +17,7 @@ class InstagramAuth {
 
     const API_TOKEN_REFRESH_URL = 'https://graph.instagram.com/refresh_access_token';
 
-    private $app_id;
+    private static $app_id;
 
     private $app_secret;
 
@@ -20,13 +28,27 @@ class InstagramAuth {
     public function __construct(\modX $modx) {
         $this->app_id = $modx->getOption('instagram_app_id');
         $this->app_secret = $modx->getOption('instagram_app_secret');
+        $this->redirect_uri = $modx->getOption('site_url');
+        $this->rest_client = $modx->getService('rest', 'rest.modRest');
+    }
+    
+    public function getLoginLink():string {
+        return self::API_OAUTH_URL . '?client_id=' . $this->app_id . '&redirect_uri=' . urlencode($this->redirect_uri) . '&scope=user_profile,user_media&response_type=code';
+    }
+    
+    /**
+     * Gets the "Long Lived" token - Expiration 2 months
+     */
+    public function getLongLivedToken(string $token) {
+        
     }
 
-    public function __get(string $name) {
-        return this[$name];
-    }
+    public function refreshAccessToken(string $token) {
+        $params = [
+            'grant_type' => 'ig_refresh_token',
+            'access_token' => $token
+        ];
 
-    public function getLoginLink() {
-        return self::API_OAUTH_URL . '?client_id=' . $this.__get('app_id') . '&redirect_uri=' . urlencode($this->__get('redirect_uri')) . '&scope=user_profile,user_media&response_type=code';
+        
     }
 }
